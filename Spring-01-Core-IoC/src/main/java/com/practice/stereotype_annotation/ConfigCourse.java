be73849a81a8a48912e348d95f3d3725c1139f4b
Suya <suyakozh@gmail.com>
2022-02-17 18:53:47 -0600

    Stereotype annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/practice/stereotype_annotation/ConfigCourse.java
@@ -0,0 +1,10 @@
+package com.practice.stereotype_annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan
+@ComponentScan(basePackages = "com.practice")
+public class ConfigCourse {
+}
---
+++ Spring-01-Core-IoC/src/main/java/com/practice/stereotype_annotation/CydeoApp.java
@@ -0,0 +1,13 @@
+package com.practice.stereotype_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigCourse.class);
+        context.getBean(Java.class).getTeachingHours();
+
+    }
+}

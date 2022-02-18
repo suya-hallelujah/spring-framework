0cc067af9210fc025255704fcf53810e37bb1bbd
Suya <suyakozh@gmail.com>
2022-02-17 19:21:21 -0600

    Wiring

---
+++ Spring-02-Core-Wiring/src/main/java/com/practice/Car.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class Car {
+    private String make;
+}
---
+++ Spring-02-Core-Wiring/src/main/java/com/practice/Person.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class Person {
+    private String name;
+    private Car car;
+}

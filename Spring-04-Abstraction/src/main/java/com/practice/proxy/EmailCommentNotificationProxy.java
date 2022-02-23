8b07fd54752aa51d6842fd54723731d254eb9f13
Suya <suyakozh@gmail.com>
2022-02-22 19:24:27 -0600

    @Qualifier

--- Spring-04-Abstraction/src/main/java/com/practice/proxy/EmailCommentNotificationProxy.java
+++ Spring-04-Abstraction/src/main/java/com/practice/proxy/EmailCommentNotificationProxy.java
@@ -1,9 +1,13 @@
 package com.practice.proxy;
 
 import com.practice.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary
+@Qualifier("EMAIL")
 public class EmailCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
--- Spring-04-Abstraction/src/main/java/com/practice/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/practice/service/CommentService.java
@@ -3,14 +3,16 @@ package com.practice.service;
 import com.practice.model.Comment;
 import com.practice.proxy.CommentNotificationProxy;
 import com.practice.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentService {
+
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository,@Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }

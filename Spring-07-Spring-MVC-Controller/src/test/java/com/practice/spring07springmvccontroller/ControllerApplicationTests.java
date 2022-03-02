475e68a605f702f72cdc4da554fb60f91e362f0c
Suya <suyakozh@gmail.com>
2022-03-01 19:21:06 -0600

    Spring MVC - Controller

---
+++ Spring-07-Spring-MVC-Controller/src/test/java/com/practice/spring07springmvccontroller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring07springmvccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

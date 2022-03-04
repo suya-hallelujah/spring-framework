815dfaea3bd30e14b33a1015bbc72c77686fc8b1
Suya <suyakozh@gmail.com>
2022-03-03 18:22:27 -0600

    Spring MVC - Model

--- Spring-08-MVC-Model/src/main/java/com/practice/controller/StudentController.java
+++ Spring-08-MVC-Model/src/main/java/com/practice/controller/StudentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -13,6 +17,18 @@ public String homepage(Model model){
     model.addAttribute("name","Cydeo");//these variable are local to this method only
     model.addAttribute("course","MVC");
 
+    String subject = "Collections";
+    model.addAttribute("subject",subject);
+
+    int studentId = new Random().nextInt();
+    model.addAttribute("studentId",studentId);
+
+    List<Integer> numbers = new ArrayList<>();
+    numbers.add(4);
+    numbers.add(5);
+    numbers.add(7);
+    numbers.add(10);
+    model.addAttribute("numbers",numbers);
 
 
     return "student/welcome";

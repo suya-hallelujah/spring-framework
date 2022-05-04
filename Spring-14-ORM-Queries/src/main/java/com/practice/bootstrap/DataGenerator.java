4d5824036f4991b34eb46bc270c96bda55336b01
Suya <suyakozh@gmail.com>
2022-05-03 20:38:50 -0500

    Queries - Derived Department & Employee

--- Spring-14-ORM-Queries/src/main/java/com/practice/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/practice/bootstrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.practice.bootstrap;
 
+import com.practice.repository.DepartmentRepository;
 import com.practice.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,10 +9,13 @@ import org.springframework.stereotype.Component;
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public DataGenerator(RegionRepository regionRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -23,6 +27,22 @@ public class DataGenerator implements CommandLineRunner {
         System.out.println("findByCountryContaining:" + regionRepository.findByCountryContaining("United"));
         System.out.println("findByCountryContainingOrderByCountry:" + regionRepository.findByCountryContainingOrderByCountry("Asia"));
         System.out.println("findTopByCountry:" + regionRepository.findTop2ByCountry("Canada"));
+
         System.out.println("-----------------REGION END-----------------");
+
+        System.out.println("-----------------DEPARTMENT START-----------------");
+
+        System.out.println("findByDepartment:" + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivisionIs:" + departmentRepository.findByDivisionIs("Outdoors"));
+        System.out.println("findDistinctTop3ByDivisionContaining:" + departmentRepository.findDistinctTop3ByDivisionContaining("Hea"));
+
+        System.out.println("-----------------DEPARTMENT END-----------------");
+
+
+
+
+
+
+
     }
 }
